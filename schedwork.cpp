8eacb99fe50f8d8fee419dd4984fedab435e14c2
Aryan Khatri <aokhatri@usc.edu>
2025-04-26 20:26:23 -0700

    Passed all tests

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,18 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+//function that recurses
+bool schedHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t day,
+    size_t worker
+);
+
+//checks if maxShifts is satisifed
+bool isValid(DailySchedule& sched, const size_t maxShifts, Worker_T worker);
+
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,10 +48,62 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
+
+    return schedHelper(avail, dailyNeed, maxShifts, sched, 0, 0);
+
+
 
 
 
 
 }
 
+bool isValid(DailySchedule& sched, const size_t maxShifts, Worker_T x)
+{
+    size_t count = 0;
+    for (DailySchedule::iterator day = sched.begin(); day != sched.end(); day++) {
+            for (vector<Worker_T>::iterator worker = day->begin(); worker != day->end(); worker++) {
+                if (*worker == x) count++;
+            }
+    }
+    return (count <= maxShifts);
+}
+
+bool schedHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t day,
+    size_t availWorker
+)
+{
+    if (day == avail.size()) {
+        return true;
+    }
+    if (availWorker == dailyNeed) {
+        return schedHelper(avail, dailyNeed, maxShifts, sched, day + 1, 0);
+    }
+    //iterates through the availWorkers in the day given
+    for (size_t i = availWorker; i < avail[day].size(); i++) {
+        if (availWorker == 0) {
+            sched.push_back(vector<Worker_T>());
+        }
+        if (avail[day][i]) {
+            //adds worker if available
+            sched[day].push_back(i);
+            //check maxShifts contraint
+            if (isValid(sched, maxShifts, i)) {
+                //moves on to next worker to be scheduled
+                if (schedHelper(avail, dailyNeed, maxShifts, sched, day, availWorker + 1)) {
+                    return true;
+                }
+            }
+            //backtrack
+            sched[day].pop_back();
+        }
+        if (sched[day].size() == 0) {
+            sched.pop_back();
+        }
+    }
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(std::string& in, const std::string& floating, std::set<std::string>& words, int num, const std::set<std::string>& dict);
 
 
 // Definition of primary wordle function
@@ -22,6 +23,54 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+
+    //helper that does recursion
+    set<string> words;
+    string trial = in;
+    wordleHelper(trial, floating, words, 0, dict);
+
+    return words;
+
+}
+
+void wordleHelper(std::string& in, const std::string& floating, set<string>& wordSet,
+    int start, const std::set<std::string>& dict)
+{
+    //check if valid size
+    if (in.find('-') == string::npos && floating.size() == 0) {
+        //check all floating are used
+        
+        if (dict.find(in) != dict.end()) {
+            wordSet.insert(in);
+        }
+        return;
+    }
+    //early pruning if number of - is less than floating letters
+    size_t emptyCount = 0;
+    for (char c : in) {
+        if (c == '-') emptyCount++;
+    }
+    if (emptyCount < floating.size()) {
+        return;
+    }
+
+    for (size_t i = start; i < in.size(); i++) {
+        if (in[i] != '-') {
+            continue;
+        }
+        for (char c = 'a'; c <= 'z'; c++) {
+            in[i] = c;
+
+            //creates new floating string that removes used letters
+            string remFloating = floating;
+            size_t pos = remFloating.find(c);
+            if (pos != string::npos) remFloating.erase(pos,1);
+            
+            wordleHelper(in, remFloating, wordSet, int(i + 1), dict);
+            in[i] = '-';
+        }
+    }
+    return;
 
 }
 
